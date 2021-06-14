0102998a97dbf8b65341f9e9b01ff5e6dbfe6b48
Erick Bernardo <erickvictor@gmail.com>
2021-06-14 10:20:06 -0400

    create Entity and CreateDto

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -6,12 +6,12 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 @Injectable()
 export class PokemonService {
   constructor(private readonly prisma: PrismaService) {}
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  create(data: CreatePokemonDto) {
+    return this.prisma.pokemon.create({ data });
   }
 
   findAll() {
-    return `This action returns all pokemon`;
+    return this.prisma.pokemon.findMany();
   }
 
   findOne(id: number) {
